6904f3352b7c597e7ca01836a08b08391a5f505b
Vivek-122005 <151515473+Vivek-122005@users.noreply.github.com>
2025-01-04 16:50:40 +0530

    Update ContactSection.tsx

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -53,7 +53,7 @@ export default function ContactSection() {
             </div>
             
             <div className="rounded-lg overflow-hidden h-64">
-              <iframe src="https://www.google.com/maps/place/Shravasti+public+school+surya+nagar+alwar/@27.5596971,76.6471665,17z/data=!3m1!4b1!4m6!3m5!1s0x39729753019bd6df:0x64b2596445927e86!8m2!3d27.5596971!4d76.6497414!16s%2Fg%2F11hd1mnhb0?hl=en&entry=ttu&g_ep=EgoyMDI1MDEwMS4wIKXMDSoASAFQAw%3D%3D"
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3549.8485961736694!2d76.6344!3d27.1789!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x396db5d6d7067c67%3A0x6f3b89ca17e15f96!2sAlwar%2C%20Rajasthan!5e0!3m2!1sen!2sin!4v1635835636547!5m2!1sen!2sin"
                 width="100%"
                 height="100%"
                 style={{ border: 0 }}
